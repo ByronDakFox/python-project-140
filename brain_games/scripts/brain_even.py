fbaeaeea10f32d6e2d07bf7e713b13bbf11cb336
Byron Ramirez <tauren62@gmail.com>
2025-08-26 20:05:32 -0500

    parte 6 proyect
    Juego par e impar

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,43 @@
+#from brain_games.cli import welcome_user
+from brain_games.scripts.brain_games import welcome_user
+import random
+
+def par_impar(name):
+
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    num = 0
+    
+
+    while num < 3:
+
+        tempNum = random.randint(1, 30)
+        print(f'Question: {tempNum}')
+
+        userGame = input('Your answer: ').strip().lower()
+
+        if tempNum % 2 == 0:
+            result = 'yes'
+        else:
+            result = 'no'
+        
+        if result == userGame:
+            print('Correct!')
+            num +=1
+        else:
+            print(f'{userGame} is wrong answer ;(. Correct answer was {result}.')
+            print(f'Let´s try again, {name}')
+            break
+    else: 
+        print(f'Congratulations, {name}!')
+
+
+
+def main():
+
+    name = welcome_user()
+    par_impar(name)
+
+if __name__ == "__main__":
+    
+    main()
